0ba7693ca6e18a86a619912f75be6e2c59f9bcda
pb3363-art <pb3363@srmist.edu.in>
2026-03-02 15:04:51 +0530

    UC1: Application Entry & Welcome Message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,13 @@
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("       PALINDROME CHECKER APP    ");
+        System.out.println("Version: 1.0.0");
+        System.out.println("Welcome to the Palindrome Checker Application!");
+
+        System.out.println("\nApplication started successfully.");
+        System.out.println("Proceeding to next use case...\n");
+    }
 }
+
